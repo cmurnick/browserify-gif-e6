e7d313c346676703c101ccc3a292efa4aa04156a
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-09-16 10:01:53 -0500

    created cards

---
+++ javascripts/data.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const printToDom = require('./dom');
+const loadGifs = require('./gifs');
+const loadCategories = require('./categories');
+
+let gifArray = [];
+
+const errorFunction = () => {
+	console.log("You broke everything");
+};
+
+//When GIF Loads
+
+const whenGifsLoad = function() {
+	gifArray = JSON.parse(this.responseText).gifs;
+	// Load categories
+	loadCategories(whenCategoriesLoad, errorFunction);
+};
+
+const whenCategoriesLoad = function(){
+	let categoryArry = JSON.parse(this.responseText).categories;
+	// combines arrays
+	combineArrays(categoryArry);
+};
+
+const combineArrays = (categories) => {
+	categories.forEach((category) => {
+		gifArray.forEach((gif) =>  {
+			if (gif.category === category.id) {
+				gif.categoryName = category.name;
+				gif.categoryDataName = category.dataName;
+			}
+		});
+	});
+	//TO DO: call the print to dom;
+	printToDom(gifArray);
+};
+
+
+// TODO: set up 'initializer' - load gifs
+const initializer = () => {
+	loadGifs(whenGifsLoad, errorFunction);
+};
+
+module.exports = initializer;
+
